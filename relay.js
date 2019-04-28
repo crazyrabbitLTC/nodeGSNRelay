7c578d06951587df5205ddc1adb1bcf41f2668e1
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-28 19:46:47 -0400

    Server returns true when relay is ready.

--- relay.js
+++ relay.js
@@ -5,31 +5,10 @@ const provider = new ethers.providers.JsonRpcProvider();
 const contractArtifact = require("./build/contracts/RelayHub.json");
 
 const privateKey =
-  "0x38e8085d0dfcd53f06b763042dde15a9f07b6b1d5fb2e7f384253050b83743a2";
-const publicKey = "0xda9d0e8b3c543a5f83025f1af527bd85703e4cca";
-//1: Deploy Contracts:
-//start Ganache
-//Relay Hub
-//userContract
-//--currently handled with truffle
+  "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
+const publicKey = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
 
-//turn into class later
-// const loadNetwork = async () => {
-//   let contract;
-//   const { networks, abi } = require("./build/contracts/RelayHub.json");
-//   const networkId = await provider.getNetwork();
-//   const accounts = await provider.listAccounts();
-//   let balance = await provider.getBalance(accounts[0]);
-//   balance = balance.toString();
-//   const chain = networkId.chainId;
-//   //   console.log(`Ethereum Chain ID: ${chain.toString()}`);
-//   //   console.log(`Balance Account[0]: ${balance}`);
-//   return (contract = new ethers.Contract(
-//     networks[chain].address,
-//     abi,
-//     provider
-//   ));
-// };
+let RelayHub;
 
 const loadNetwork = async () => {
   const thing = new RelayHubClass(provider, contractArtifact);
@@ -50,7 +29,10 @@ class RelayHubClass {
       accounts: null,
       chain: null,
       wallet: null,
-      instanceWithSigner: null
+      instanceWithSigner: null,
+      relayStaked: {},
+      relayAdded: {},
+      relayReady: false,
     };
   }
 
@@ -95,48 +77,76 @@ class RelayHubClass {
     let fee = utils.parseEther("1");
 
     instanceWithSigner.on("Staked", (relay, value, event) => {
-      console.log("On Staked");
-      console.log(relay);
-      console.log(utils.toString(value));
+      const relayStaked = {
+        relay,
+        value
+      };
 
-      //console.log(event);
+      value = utils.formatEther(value, { commify: true });
+      console.log(
+        `Relay Staked: ${relay}, and stake: ${value} Ether.`
+      );
+      this.state = { ...this.state, relayStaked };
+
+      registerRelay();
     });
 
-    instanceWithSigner.on(
-      "RelayAdded",
-      (sender, owner, fee, stake, unstakeDelay, url) => {
-        console.log("On Registered");
+    instanceWithSigner.on("RelayAdded",(sender, owner, fee, stake, unstakeDelay, url) => {
+        const relayAdded = {
+          sender,
+          owner,
+          fee,
+          stake,
+          unstakeDelay,
+          url
+        };
+        console.log("Relay Registered");
         fee = utils.formatEther(fee, { commify: true });
         stake = utils.formatEther(stake, { commify: true });
         unstakeDelay = utils.formatEther(unstakeDelay);
         console.log(
           `Relay Added: Sender: ${sender} Owner: ${owner} Fee: ${fee} Stake: ${stake} Delay: ${unstakeDelay} URL: ${url} `
         );
+
+        this.state = { ...this.state, relayAdded, relayReady: true };
       }
     );
+
+    const registerRelay = async () => {
+      try {
+        console.log("register");
+        const tx = await instanceWithSigner.register_relay(
+          fee,
+          "http://localhost:3000",
+          "0x0000000000000000000000000000000000000000"
+        );
+
+        await tx.wait();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const stakeRelay = async () => {
     //first Stake
     try {
-      const tx = await instanceWithSigner.stake(publicKey, 10, {
-        value: stakeWei
-      });
-      await tx.wait();
-    } catch (error) {
-      console.error;
+        const tx = await instanceWithSigner.stake(publicKey, 5, {
+          value: stakeWei
+        });
+        await tx.wait();
+      } catch (error) {
+        console.error;
+      }
     }
 
-    try {
-      console.log("register");
-      const tx = await instanceWithSigner.register_relay(
-        fee,
-        "http://localhost:3000",
-        "0x0000000000000000000000000000000000000000"
-      );
-
-      await tx.wait();
-    } catch (error) {
-      console.log(error);
-    }
+    stakeRelay();
   }
+
+  get isRelayReady() {
+      return this.state.relayReady;
+  }
+
+
 }
 
 //2: Fund and initialize hub and contract
@@ -165,8 +175,12 @@ app.get("/networks", async (req, res) => {
   res.send(networks);
 });
 
+app.get("/ready", async (req, res) => {
+    res.send(RelayHub.isRelayReady);
+})
+
 app.listen(3000, async () => {
-  const RelayHub = new RelayHubClass(provider, contractArtifact);
+RelayHub = new RelayHubClass(provider, contractArtifact);
   await RelayHub.init();
   RelayHub.registerRelay("1", "1");
   console.log("app listening on port 3000!");
