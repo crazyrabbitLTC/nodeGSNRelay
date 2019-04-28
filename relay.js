5a2ee83e6762d249c4f71627134667d3d3ec9cc3
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-28 09:48:56 -0400

    Demo adding accounts

--- relay.js
+++ relay.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const ethers = require('ethers');
+
+const provider = new ethers.providers.JsonRpcProvider();
+
+
 
 app.use((req, res, next) => {
     console.log('Request Time: ', Date.now());
@@ -9,6 +14,10 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     res.send('An alligator approaches!');
+});
+
+app.get('/accounts', (req, res) => {
+    provider.listAccounts().then(result => res.send(result));
 });
 
 
