31b3b8cbc191ffc247756110a8199a6e88995ef2
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-28 18:40:37 -0400

    Deposit fund into relayhub.

--- relay.js
+++ relay.js
@@ -4,6 +4,8 @@ const ethers = require("ethers");
 const provider = new ethers.providers.JsonRpcProvider();
 const contractArtifact = require("./build/contracts/RelayHub.json");
 
+const privateKey =
+  "0x38e8085d0dfcd53f06b763042dde15a9f07b6b1d5fb2e7f384253050b83743a2";
 //1: Deploy Contracts:
 //start Ganache
 //Relay Hub
@@ -34,7 +36,7 @@ const loadNetwork = async () => {
 };
 
 class RelayHubClass {
-  constructor(provider, contractArtifact) {
+  constructor(provider, contractArtifact, privKey) {
     this.provider = provider;
     this.contractArtifact = contractArtifact;
     //this.init();
@@ -45,19 +47,27 @@ class RelayHubClass {
       networks: null,
       networkId: null,
       accounts: null,
-      chain: null
+      chain: null,
+      wallet: null,
+      instanceWithSigner: null
     };
   }
 
   async init() {
-    const networkId = await provider.getNetwork();
-    const accounts = await provider.listAccounts();
+    const networkId = await this.provider.getNetwork();
+    const accounts = await this.provider.listAccounts();
+    const wallet = new ethers.Wallet(privateKey, this.provider);
     const { networks, abi } = this.contractArtifact;
     const chain = networkId.chainId;
     const instance = new ethers.Contract(
       networks[chain].address,
       abi,
-      provider
+      this.provider
+    );
+    const instanceWithSigner = new ethers.Contract(
+      networks[chain].address,
+      abi,
+      wallet
     );
 
     this.state = {
@@ -67,15 +77,37 @@ class RelayHubClass {
       networks,
       abi,
       chain,
-      instance
+      instance,
+      instanceWithSigner,
+      wallet
     };
+    console.log("Init Worked");
   }
 
-  async fundRelay(amount) {
-    const { instance } = this.state;
+  async registerRelay(stake, deposit) {
+    const { instanceWithSigner, accounts} = this.state;
+    const {utils} = ethers;
+    // let balance = await provider.getBalance(accounts[0]);
+    // balance = balance.toString();
+    // balance = balance.toNumber();
 
-    let balance = await instance.balanceOf(this.state.accounts[0]);
-    console.log(`Balance of ${balance.toString()}`);
+ 
+    let wei = utils.parseEther(deposit);
+    console.log("Wei: ", wei);
+    //first deposit
+    try {
+      const tx = await instanceWithSigner
+        .deposit({value: wei });
+        console.log(tx);
+        console.log("WAit for transaction to mine");
+        await tx.wait();
+        console.log("Transaction mined");
+    } catch (error) {
+        console.log(error)
+    }
+
+    //let balance = await instance.balanceOf(this.state.accounts[0]);
+    //console.log(`Balance of ${balance.toString()}`);
   }
 }
 
@@ -108,6 +140,6 @@ app.get("/networks", async (req, res) => {
 app.listen(3000, async () => {
   const RelayHub = new RelayHubClass(provider, contractArtifact);
   await RelayHub.init();
-  RelayHub.fundRelay(1);
+  RelayHub.registerRelay('1','1');
   console.log("app listening on port 3000!");
 });
