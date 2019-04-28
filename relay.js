048df67c916f44aa1d38c3a4be08481132d4f263
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-28 09:40:39 -0400

    Install Nodemon

--- relay.js
+++ relay.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Request Time: ', Date.now());
+    next();
+});
+
+
 app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 
-app.use((req, res, next) => {
-    console.log('Time: ', Date.now());
-    next();
-});
+
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
