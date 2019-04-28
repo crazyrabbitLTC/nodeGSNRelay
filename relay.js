7bc32549e706f1850fb9d1511215e818dc46c49e
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-28 09:38:56 -0400

    add basic logger

--- relay.js
+++ relay.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 
+app.use((req, res, next) => {
+    console.log('Time: ', Date.now());
+    next();
+});
+
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
