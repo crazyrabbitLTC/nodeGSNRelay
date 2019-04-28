0e1b16b6a310a6885118efd4db2d5081290ae98f
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-28 09:55:22 -0400

    Add Solc

--- relay.js
+++ relay.js
@@ -4,7 +4,18 @@ const ethers = require('ethers');
 
 const provider = new ethers.providers.JsonRpcProvider();
 
+//1: Deploy Contracts: 
+    //Relay Hub
+    //userContract
 
+//2: Fund and initialize hub and contract
+    //Fund Relay for userContract
+    //init userContract for relay
+
+//3: Submit transactions for User
+    //Receive Transaction
+    //Check if can relay
+    //
 
 app.use((req, res, next) => {
     console.log('Request Time: ', Date.now());
